84d21dc947b46bd2794aec823f757ede27ff0351
Matteo Ceserani <ceserani@gmail.com>
2023-03-23 20:23:58 +0100

    Get & Post handled

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import sqlite3 from 'sqlite3';
+
+const app = express();
+
+// Create a database users.db
+const db = new sqlite3.Database('users.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the SQlite database.');
+});
+
+// Create a table users if it does not exist
+db.run('CREATE TABLE IF NOT EXISTS user (name TEXT, surname TEXT, matr INTEGER)');
+
+// Handle GET requests to the root: get data from database with given matr
+app.get('/:matr', (req, res) => {
+    db.get('SELECT * FROM user WHERE matr = ?', [req.params.matr], (err, row) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        if (!row) {
+            res.send(`A user with matr ${req.params.matr} does not exist`);
+        } else {
+            res.send(row);
+        }
+    });
+});
+
+// Handle POST requests to the root: insert data into database
+app.post('/:name/:surname/:matr', (req, res) => {
+    db.get('SELECT matr FROM user WHERE matr = ?', [req.params.matr], (err, row) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        if (!row) {
+            db.run('INSERT INTO user (name, surname, matr) VALUES (?, ?, ?)', [req.params.name, req.params.surname, req.params.matr], function(err) {
+                if (err) {
+                    return console.error(err.message);
+                }
+                res.send(`User with matr ${req.params.matr} added to database`);
+            });
+        } else {
+            res.send(`A user with matr ${req.params.matr} already exists`);
+        }
+    });
+});
+
+// Listen on port 3000
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
